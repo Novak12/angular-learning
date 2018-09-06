1cc67ba31c5857814e8c0c8b1f81aebe8e11af26
novak12 <novak12@163.com>
2018-09-06 22:57:08 +0800

    add proprty and array sample

--- src/ng-app/src/app/components/news/news.component.ts
+++ src/ng-app/src/app/components/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-news',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-  news = 'this is app-news'
+  //property define
+  news = 'this is app-news';
+  public name: string;
+  private age: number = 20;
+  protected address: string;//只能在该类及子类中使用
+  Message: string = 'hello';
+  public cities: any[]
   constructor() {
+    this.address = 'shanghai';
+    this.name = 'jonny';
+    this.Message = '<h3>add inner html</h3>';
 
+    this.cities = ['shanghai', 'beijing', 'new york'];
   }
 
   ngOnInit() {
